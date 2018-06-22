fe46f54e2da7427521d4c02f0b9f54f737942e4b
xcartmods <xcartmods@users.noreply.github.com>
2018-06-22 06:06:35 +0100

    Update index.php

--- index.php
+++ index.php
@@ -574,7 +574,7 @@ $(function(){
 <div class="row align-items-center">
 
 <div class="col-md-12 col-lg-3 mb-4 text-center text-lg-left order-1 order-lg-1">
-<h3 class="mb-0"><a href="index.php"><span style="font-size:20px;"><span class="fa-stack fa-lg mb-1"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-file-text-o fa-stack-1x fa-inverse"></i></span></span> <b>File Manager</b></a></h3>
+<h3 class="mb-0"><span style="font-size:20px;"><span class="fa-stack fa-lg mb-1"><i class="fa fa-square fa-stack-2x"></i><i class="fa fa-file-text-o fa-stack-1x fa-inverse"></i></span></span> <a href="index.php"><b>File Manager</b></a></h3>
 </div>
 
 <div class="col-md-12 col-lg-3 mb-4 order-3 order-lg-2">
